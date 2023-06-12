66711c37c29273874823dd4b7386515dd67460da
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2023-06-12 15:51:44 -0400

    try new scripts

---
+++ coverage.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#  Copyright (C) 2018-2020 LEIDOS.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#  use this file except in compliance with the License. You may obtain a copy of
+#  the License at
+# 
+#  http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations under
+#  the License.
+
+# script to run tests, generate test-coverage, and store coverage reports in a place
+# easily accessible to sonar. Test names should follow convention run<pluginName>Tests
+
+cd /home/jpo-cvdp
+mkdir test_results
+
+cd /home/jpo-cvdp/cv-lib/build/
+./cv-lib --gtest_output=xml:../../../test_results/
+cd /home/jpo-cvdp/
+mkdir coverage
+cd /home/jpo-cvdp/
+gcovr --sonarqube iguration/coverage/coverage.xml -s -f iguration/ -r .
+
+cd /home/jpo-cvdp/kafka_test/build/
+./kafka_test --gtest_output=xml:../../test_results/
+cd /home/jpo-cvdp/kafka_test/
+mkdir coverage
+cd /home/jpo-cvdp/
+gcovr --sonarqube kafka_clients/coverage/coverage.xml -s -f kafka_clients/ -r .
+
+cd /home/jpo-cvdp/build/
+./jpo-cvdp --gtest_output=xml:../../../test_results/
+cd /home/jpo-cvdp/
+mkdir coverage
+cd /home/jpo-cvdp/
+gcovr --sonarqube /coverage/coverage.xml -s -f / -r .
+
